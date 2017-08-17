314ec674466af7ee1391f03ae745f9720e949588
Brian Peterson <bpeterso2000@yahoo.com>
2017-08-17 01:29:22 -0400

    Consolidate Python version tests

--- jsonconfig/_compat.py
+++ jsonconfig/_compat.py
@@ -7,12 +7,9 @@ PY35_PLUS = VER.major > 2 and not (VER.major == 3 and VER.minor < 5)
 
 
 def get_parameters(funct):
-    if PY2:
-        return inspect.getargspec(funct).args
-    return inspect.signature(funct).parameters
+    return (inspect.getargspec(funct).args if PY2
+            else inspect.signature(funct).parameters)
 
 
-if PY35_PLUS:
-    OPEN_PARAMETERS = get_parameters(open)
-else:
-    OPEN_PARAMETERS = ['name', 'mode', 'buffering']
+OPEN_PARAMETERS = (get_parameters(open) if PY35_PLUS
+                   else ['name', 'mode', 'buffering'])
