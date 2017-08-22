abf1a3f7db58f751b902e7986b6575427c13411b
Brian Peterson <bpeterso2000@yahoo.com>
2017-08-21 21:19:17 -0400

    Simplify EnvironAttrDict & KeyringAttrDict names

--- jsonconfig/__init__.py
+++ jsonconfig/__init__.py
@@ -6,9 +6,9 @@ from click.termui import hidden_prompt_func as getpass
 
 from .core import Config
 from .appdirs import get_filename
-from .environs import EnvironAttrDict
+from .environs import Environ
 from .jsonutils import to_json_file, from_json_file, to_json, from_json
-from .keyrings import set_keyring, KeyringAttrDict
+from .keyrings import set_keyring, Keyring
 from .kwargs import group_kwargs_by_funct
 from .errors import (
     JsonConfigError,
