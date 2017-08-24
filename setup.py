c7444fe24cf49e4800d919dcba09e713188be8f7
Brian Peterson <bpeterso2000@yahoo.com>
2017-08-24 16:47:38 -0400

    Add metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import re
 import ast
+from codecs import open
 from setuptools import setup
 
 requirements = ['click==6.7', 'python-box==3.0.1', 'keyring==10.4.0']
@@ -11,21 +12,33 @@ with open('jsonconfig/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
     name='jsonconfig-tool',
     version=version,
+    description='Configuration made easy: JSON, encrypted, envvars, etc.',
+    long_description=readme,
+    license='MIT',
     author='Brian Peterson',
     author_email='bpeterso2000@yahoo.com',
     keywords='config configuration json encryption passwords simple easy',
     url='http://github.com/json-transformations/jsonconfig',
     packages=['jsonconfig'],
-    description='Configuration made easy: JSON, encrypted, environ vars, etc.',
     classifiers=['License :: OSI Approved :: MIT License',
+                 'Intended Audience :: Developers',
+                 'Natural Language :: English',
                  'Topic :: Software Development',
                  'Topic :: Utilities',
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
                  'Environment :: Win32 (MS Windows)',
                  'Environment :: MacOS X',
                  'Operating System :: Microsoft :: Windows',
