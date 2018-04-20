ffad49e67ea33b831d07047d8de9e4adfae7f366
Brian Peterson <bpeterso2000@yahoo.com>
2018-04-19 21:15:27 -0400

    Add dbus-python requirement for Linux platforms; closes #12

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ import ast
 from codecs import open
 from setuptools import setup
 
-requirements = ['click==6.7', 'python-box==3.1.1', 'keyring==11.0.0']
-test_requirements = ['flake8==3.5.0', 'pytest-cov==2.5.1', 'sphinx',
-                     'tox==2.9.1']
-
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('jsonconfig/__init__.py', 'rb') as f:
@@ -18,6 +14,7 @@ with open('README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='jsonconfig-tool',
+    python_requires='>=2.7',
     version=version,
     description='Configuration made easy: JSON, encrypted, envvars, etc.',
     long_description=readme,
@@ -27,26 +24,19 @@ setup(
     keywords='config configuration json encryption passwords simple easy',
     url='http://github.com/json-transformations/jsonconfig',
     packages=['jsonconfig'],
-    classifiers=['License :: OSI Approved :: MIT License',
-                 'Intended Audience :: Developers',
-                 'Natural Language :: English',
-                 'Topic :: Software Development',
-                 'Topic :: Utilities',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6',
-                 'Environment :: Win32 (MS Windows)',
-                 'Environment :: MacOS X',
-                 'Operating System :: Microsoft :: Windows',
-                 'Operating System :: MacOS :: MacOS X',
-                 'Operating System :: POSIX',
-                 'Operating System :: Unix',
-                 ],
-    install_requires=requirements,
-    tests_require=test_requirements
+    classifiers=[
+         'License :: OSI Approved :: MIT License',
+         'Intended Audience :: Developers',
+         'Topic :: Software Development',
+         'Topic :: Utilities',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'click==6.7',
+        'python-box==3.1.1',
+        'keyring==12.0.1',
+        'dbus-python==1.2.4; platform_system=="Linux"',
+    ],
 )
